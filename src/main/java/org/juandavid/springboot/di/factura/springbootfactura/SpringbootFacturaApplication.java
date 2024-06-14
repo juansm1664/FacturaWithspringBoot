06ba76c866ad93055ca9ace53c5cb672c7524f18
JuanDavidSerna <jserna@cloudlabs.co>
2024-06-13 20:39:57 -0500

    practicandoDIFactura

---
+++ src/main/java/org/juandavid/springboot/di/factura/springbootfactura/SpringbootFacturaApplication.java
@@ -0,0 +1,13 @@
+package org.juandavid.springboot.di.factura.springbootfactura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFacturaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFacturaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/juandavid/springboot/di/factura/springbootfactura/SpringbootFacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.juandavid.springboot.di.factura.springbootfactura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFacturaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
